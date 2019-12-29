dde18dd66b1bf31d8723ab1e680070aab5c63eb3
streamZYYang <458082976@qq.com>
2019-12-29 16:56:03 +0800

    add python3 vitualenv & qt5 install

---
+++ pyqt5_install.bat
@@ -0,0 +1,4 @@
+pip install pyqt5
+REM install qtdesigner
+REM designer have been added in pyqt5-tools after python3.5
+pip install pyqt5-tools
---
+++ virtualenv_install.bat
@@ -0,0 +1,4 @@
+REM python -m pip install virtualenv
+pip install virtualenv
+
+REM create an env python -m virtual <env_name>, or virtual <env_name>
